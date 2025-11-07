fd3282bb263c86df93dea3cabf15ec7feccbdc58
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-11-07 13:21:51 -0300

    Base de Datos

--- Server/src/DataBase/db.js
+++ Server/src/DataBase/db.js
@@ -8,7 +8,18 @@ const db= new SQLite.Database(dbUbicacion, (Error) => {
         console.error('Error al Conectar a la Base de Datos: ⛔', Error);
     } else {
         console.log('Conexión a la Base de Datos establecida ✅');
-        asasasa
+        db.run(`CREATE TABLE IF NOT EXISTS Usuarios (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nombre TEXT NOT NULL,
+            Email TEXT NOT NULL UNIQUE,
+            Password TEXT NOT NULL
+        )`, (Error) => {
+            if (Error) {
+                console.error('Error al crear la tabla de usuarios: ⛔', Error);
+            } else {
+                console.log('Tabla de usuarios creada ✅');
+            }
+        }); 
     }
 });
 
